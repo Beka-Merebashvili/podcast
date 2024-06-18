3633ee6cf97bda7f8b85bbc4ec27bf1e3b3946f2
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-06-18 16:19:39 +0400

    add custom colors

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const LeftSidebar = () => {
-    return ( 
-        <section className="left_sidebar">
-            <nav className="flex flex-col gap-6">
-                <Link href="/"></Link>
-            </nav>
-        </section>
-     );
-}
- 
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo"
+        width={23} height={27} />
+        </Link>
+      </nav>
+    </section>
+  );
+};
+
 export default LeftSidebar;
