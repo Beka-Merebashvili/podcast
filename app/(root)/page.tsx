c2541405d0180e78ca4269cdf624ad745e281a64
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-06-18 18:55:32 +0400

    add trandig podcsts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
+import { podcastData } from "@/constants";
+import PodcastCard from "../../components/PodcastCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+      <div className="podcast_grid">
+        {podcastData.map(({ id, title, description, imgURL }) => (
+          <PodcastCard
+            key={id}
+            imgUrl={imgURL}
+            title={title}
+            description={description}
+            podcastId={id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
