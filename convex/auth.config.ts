cecf0d84d86d11ada8bec19944b0c6e8636bbcca
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-06-25 11:48:06 +0400

    add auth config

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://humane-ibex-16.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
