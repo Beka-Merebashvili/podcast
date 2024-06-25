20223611f95cdff0fc8b719ca85b0643e60b370e
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-06-25 13:31:44 +0400

    finish clerk and convex setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,20 @@
+"use client"
+
 import { podcastData } from "@/constants";
 import PodcastCard from "../../components/PodcastCard";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
+  // const tasks = useQuery(api.tasks.get);
   return (
     <div>
       <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
 
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-    </div>
+      {/* <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      {tasks?.map(({ _id, text }:any) => <div key={_id}>{text}</div>)}
+    </div> */}
 
 
       <div className="podcast_grid">
